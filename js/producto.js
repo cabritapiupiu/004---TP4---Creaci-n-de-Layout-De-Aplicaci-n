7aff1fe0529cd229fb0094a0a4b10c1626fc8d55
cabritapiupiu <luca39760@gmail.com>
2024-08-13 20:25:50 -0300

    hola5 hola5

--- js/producto.js
+++ js/producto.js
@@ -1,23 +1,24 @@
 fetch('../json/local.json')
-    .then(response => response.json())
-    .then(data => {
-        data.forEach(e => {
-            e.stock.forEach(stock => {
-                if (localStorage.getItem("id_prenda") == stock.id) {
-                    document.querySelector(".pic").setAttribute("src", stock.image);
-                    console.log(stock.descuento)
-                      document.querySelector('main').innerHTML += `
-                        <div class="datos">
-                        <h2> ${stock.prenda}</h2>
-                        <div class="ofertas">
-                <p class="f30 descuento ">$${stock.descuento}</p>
-                <p class=" f20 precio_viejo">$${stock.precio_viejo}</p>
-                <p>${stock.description}</p>
-                
-            </div>
-        </div>
-                    `
-                }
-            });
-        });
-    });
+.then(response => response.json())
+.then(data => {
+data.forEach(e => {
+e.stock.forEach(stock => {
+if (localStorage.getItem("id_prenda") == stock.id) {
+document.querySelector(".pic").setAttribute("src", stock.image);
+console.log(stock.descuento)
+document.querySelector('.datos').innerHTML += `
+
+    <h1> ${stock.prenda}</h1>
+    <div class="ofertas">
+        <p class="f30 descuento ">$${stock.descuento}</p>
+        <p class=" f20 precio_viejo">$${stock.precio_viejo}</p>
+    </div>
+    <div class="description">
+        <p>${stock.description}</p>
+    </div>
+
+`
+}
+});
+});
+});
